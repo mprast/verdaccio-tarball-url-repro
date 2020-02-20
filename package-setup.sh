1b8f62b634fc1ca2dc20162c1ece3a23cf1bfeae
Matthew Prast <mprast@gmail.com>
2020-02-20 06:57:04 +0000

    safeguard against double runs

--- package-setup.sh
+++ package-setup.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 set -o errexit
 
-yarn config set nprmRegistryServer "http://localhost:4873"
+yarn config set npmRegistryServer "http://localhost:4873"
 cd packageB
+sed -i 's/  "version": "0.0.2"/  "version": "0.0.1"/g' package.json #makes this script idempotent
 yarn npm publish 
 sed -i 's/  "version": "0.0.1"/  "version": "0.0.2"/g' package.json #roll the version of packageB so 'yarn install' doesn't just pick up the workspace copy
 cd ..
